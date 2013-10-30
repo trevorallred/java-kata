a7a6c6bfeb6ee950a46652923caa3672399a52e7
Trevor Allred <trevorallred@gmail.com>
2013-10-30 08:47:07 -0700

    fixed second test

--- PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
+++ PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
@@ -1,9 +1,14 @@
 package com.trevorallred.kata.PrimeFactors;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static List<Integer> generate(int number) {
-        return null;
+        ArrayList<Integer> primes = new ArrayList<>();
+        if (number == 2) {
+            primes.add(2);
+        }
+        return primes;
     }
 }
