c2f037ac564b9a99ee95071304dc5669c0a273a5
Trevor Allred <trevorallred@gmail.com>
2013-10-30 09:30:13 -0700

    fix fourth failing test

--- PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
+++ PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
@@ -7,6 +7,10 @@ public class PrimeFactors {
     public static List<Integer> generate(int number) {
         List<Integer> primeFactors = new ArrayList<>();
 
+        if (number % 2 == 0) {
+            primeFactors.add(2);
+            number /= 2;
+        }
         if (number > 1) {
             primeFactors.add(number);
         }
