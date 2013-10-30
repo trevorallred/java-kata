5d0067bfe6b040d001f9c63d8869ee3943a947a2
Trevor Allred <trevorallred@gmail.com>
2013-10-30 00:41:38 -0700

    add failing test

--- PrimeFactors/src/test/java/com/trevorallred/kata/PrimeFactors/PrimeFactorsTest.java
+++ PrimeFactors/src/test/java/com/trevorallred/kata/PrimeFactors/PrimeFactorsTest.java
@@ -1,4 +1,18 @@
 package com.trevorallred.kata.PrimeFactors;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Write a class named “PrimeFactors” that has one static method: generate.
+ * The generate method takes an integer argument and returns a List<Integer>.
+ * That list contains the prime factors in numerical sequence.
+ */
 public class PrimeFactorsTest {
+
+    @Test
+    public void testOne() throws Exception {
+        assertEquals(list(), PrimeFactors.generate(1);
+    }
 }
