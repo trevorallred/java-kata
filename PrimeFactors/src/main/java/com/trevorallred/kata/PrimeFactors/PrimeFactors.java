00581950e9cfd09e8b6a2c44ff6ffb483175337c
Trevor Allred <trevorallred@gmail.com>
2013-10-30 10:30:59 -0700

    fixed failing test for 147

--- PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
+++ PrimeFactors/src/main/java/com/trevorallred/kata/PrimeFactors/PrimeFactors.java
@@ -1,34 +1,22 @@
 package com.trevorallred.kata.PrimeFactors;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class PrimeFactors {
-    private static Set<Integer> primes = new HashSet<>();
-
     public static List<Integer> generate(int number) {
         List<Integer> primeFactors = new ArrayList<>();
 
-        primes.add(2);
-        primes.add(3);
-
-        number = reduceNumberByFactor(number, primeFactors);
-        if (number > 1) {
-            primeFactors.add(number);
-        }
-        return primeFactors;
-    }
-
-    private static int reduceNumberByFactor(int number, List<Integer> primeFactors) {
-        for (Integer prime : primes) {
-            if (number % prime == 0) {
-                primeFactors.add(prime);
-                number /= prime;
-                number = reduceNumberByFactor(number, primeFactors);
+        int divisor = 2;
+        while (divisor <= number) {
+            if (number % divisor == 0) {
+                primeFactors.add(divisor);
+                number /= divisor;
+            } else {
+                divisor++;
             }
         }
-        return number;
+
+        return primeFactors;
     }
 }
