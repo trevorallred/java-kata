9c5c506c3ec076d54b013e35ce249cb4b76e97e9
Trevor Allred <trevorallred@gmail.com>
2013-10-30 08:44:31 -0700

    add second failing test

--- PrimeFactors/src/test/java/com/trevorallred/kata/PrimeFactors/PrimeFactorsTest.java
+++ PrimeFactors/src/test/java/com/trevorallred/kata/PrimeFactors/PrimeFactorsTest.java
@@ -2,6 +2,7 @@ package com.trevorallred.kata.PrimeFactors;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
@@ -10,15 +11,26 @@ import static junit.framework.Assert.assertEquals;
  * Write a class named “PrimeFactors” that has one static method: generate.
  * The generate method takes an integer argument and returns a List<Integer>.
  * That list contains the prime factors in numerical sequence.
+ * "Prime Factorization" is finding which prime numbers multiply together to make the original number.
+ * http://www.mathsisfun.com/prime-factorization.html
  */
 public class PrimeFactorsTest {
+
+    private List<Integer> list(int... primes) {
+        List<Integer> primeList = new ArrayList<>();
+        for (int prime : primes) {
+            primeList.add(prime);
+        }
+        return primeList;
+    }
 
     @Test
     public void testOne() throws Exception {
         assertEquals(list(), PrimeFactors.generate(1));
     }
 
-    private List<Integer> list() {
-        return null;
+    @Test
+    public void testTwo() throws Exception {
+        assertEquals(list(2), PrimeFactors.generate(2));
     }
 }
